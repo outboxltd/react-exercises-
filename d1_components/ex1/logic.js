87b37578da26cae01f8bf8fede38ae7b60e2f624
outboxltd <benatiya@gmail.com>
2019-06-23 10:16:39 +0300

    classstar1

--- d1_components/ex1/logic.js
+++ d1_components/ex1/logic.js
@@ -1,4 +1,26 @@
+class HelloWorldComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li>first</li>
+                    <li>second</li>
+                    <li>third</li>
+                    <li>forth</li>
+
+                </ul>
+            </div>
+        );
+    }
+}
+
+
+
 ReactDOM.render(
-    <div>Hello gilad</div>,
+    <div className="yalla">
+        
+        <HelloWorldComponent/>,
+
+    </div>,
     document.getElementById("root")
 );
