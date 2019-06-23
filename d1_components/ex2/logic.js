ad6a06c899d67c209060e3ee531b8c8ac80ee988
outboxltd <benatiya@gmail.com>
2019-06-23 10:59:40 +0300

    ex2

--- d1_components/ex2/logic.js
+++ d1_components/ex2/logic.js
@@ -1,4 +1,57 @@
+class BigBox extends React.Component {
+    render() {
+        return (
+            <div className="BigBox">
+                
+                <Midlebox/>
+            </div>
+        );
+    }
+}
+
+
+class Midlebox extends React.Component {
+    render() {
+        return (
+            <div className="Midlebox">
+               <TiradBox/> 
+                
+            </div>
+        );
+    }
+}
+
+class TiradBox extends React.Component {
+    render() {
+        return (
+            <div className="TiradBox">
+               <SmallBox/> 
+               <SmallBox/> 
+
+                
+            </div>
+        );
+    }
+}
+
+
+class SmallBox extends React.Component {
+    render() {
+        return (
+            <div className="SmallBox">
+                
+                
+            </div>
+        );
+    }
+}
+
+
 ReactDOM.render(
-    <div>Hello World</div>,
+    <div>
+        
+        <BigBox/>,
+
+    </div>,
     document.getElementById("root")
 );
