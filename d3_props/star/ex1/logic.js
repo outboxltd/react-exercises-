7b9e0c078b22260dca8e7eb0784f8454428324fa
outboxltd <benatiya@gmail.com>
2019-06-25 18:30:51 +0300

    ex1star

--- d3_props/star/ex1/logic.js
+++ d3_props/star/ex1/logic.js
@@ -1,30 +1,68 @@
-function render() { 
-    ReactDOM.render(
-        <div>
+class TheBox extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-            <a href="{data.linkAddress}"> {data.linkText}</a>
-            <input type="range" min={data.min} max={data.max}></input>
-            <button>{data.buttonText}</button>
+  render() {
+    let style = {
+      "height": this.props.gova,
+      "width": this.props.rohav,
+      "backgroundColor": this.props.bgc
+    };
+
+
+    return (
+      <div >
+
+
+        <div className={this.props.CCC}>
+
+          <img src={this.props.src} alt={this.props.text} className="pico" ></img>
+
+          <h3 className="center"> {this.props.title}</h3>
+
+          <p className="center"> {this.props.blabla} </p>
 
 
 
+        </div>
 
 
-
-        </div>,
-        document.getElementById("root")
+      </div>
     );
-    
+  }
 }
 
-var data = {
-    linkAddress: "https://www.google.com/",
-    linkText: "google",
-    min: -5,
-    max: 5,
-   
-    buttonText: "click me"
-}; 
+
+
+// the app
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="cont">
+        <TheBox text="fresh" CCC="fresh" src="https://image.flaticon.com/icons/svg/874/874970.svg" title="Ready to travel" blabla="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores fugiat architecto pariatur alias"/>
+
+        <TheBox text="calm" CCC="calm" src="https://image.flaticon.com/icons/svg/875/875011.svg" title="select the date" blabla="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores fugiat architecto pariatur alias" />
+
+        <TheBox text="advanturs" CCC="advanturs" src="https://image.flaticon.com/icons/svg/874/874997.svg" title="feels like home" blabla="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores fugiat architecto pariatur alias" />
+
+
+
+
+      </div>
+    );
+  }
+}
+
+// the app
+
+
+function render() {
+  ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+  );
+}
 
 render();
-
