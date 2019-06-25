b4fbfcc585e8901f1338fff3dd9c3dd8c8df165a
outboxltd <benatiya@gmail.com>
2019-06-25 12:28:42 +0300

    bigbox

--- d3_props/class/ex3/logic.js
+++ d3_props/class/ex3/logic.js
@@ -1,27 +1,35 @@
-
-class Laser extends React.Component {
-    activateLasers(){ 
-        document.getElementById("greating").innerHTML = "hello world";
-        console.log("clicked");
-    }
-
-
-      render() {
-         return (
-            <div> 
-            <button onClick={this.activateLasers}> Activate Lasers</button>
-            <div id="greating"></div>
+class Bigbox extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className={this.props.class + " box"}> 
+                {this.props.children}
+                  
+                     </div>
             </div>
-            );
+        );
+    }
+}
 
 
-    } }
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <Bigbox class="green">
+                <Bigbox class="blue">
+                    <Bigbox class="pink">
+                    <Bigbox class="purple"> </Bigbox>
+                    <Bigbox class="purple"> </Bigbox>
+                    </Bigbox>
+                </Bigbox>
+            </Bigbox>);
+    }
+}
 
 
 ReactDOM.render(
-    <div className="yoo">
-        <Laser />
-    </div>,
+    <App />,
     document.getElementById("root")
 );
-
