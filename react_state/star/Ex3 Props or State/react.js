5bba57d2446fe01250f7042681186ceac1c9c61b
outboxltd <benatiya@gmail.com>
2019-06-30 16:30:52 +0300

    Ex3 Props or State

---
+++ react_state/star/Ex3 Props or State/react.js
@@ -0,0 +1,47 @@
+class SecondComponent extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        isVisible : true,
+        text : "isVisible  with state",
+    };    
+  }
+
+ 
+  render() {
+      return(
+          <div className="YallaApp">
+
+            <h4>  {this.props.text}  </h4>    
+            <h4>   {this.state.text} </h4> 
+
+             
+
+          
+          </div>
+      )
+  }  
+}
+
+class App extends React.Component {
+ 
+  render() {
+      return(
+          <div className="YallaApp">
+
+          <SecondComponent   text="isVisible  with prop"/>
+     
+             
+
+          
+          </div>
+      )
+  }  
+}
+
+
+
+
+
+
+ReactDOM.render(<App />,document.getElementById('app'))
