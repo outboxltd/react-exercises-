e5bfee9a4521b9a50f0aa0cf7f8699c34ca96178
outboxltd <benatiya@gmail.com>
2019-07-02 17:23:07 +0300

    ex3

---
+++ lifecycle/star/Programming Experience/ex2.jsx
@@ -0,0 +1,62 @@
+class Radio extends React.Component {
+    constructor(props) {
+      super(props);
+      this.isProgramer = this.isProgramer.bind(this);
+    }
+  
+    isProgramer(e) {
+      this.props.programer(e.target.value);
+    }
+  
+    render() {
+      return (
+        <div>
+          <input
+            type="radio"
+            checked={this.props.checked}
+            name={this.props.name}
+            value={this.props.value}
+            onClick={this.isProgramer}
+          />{" "}
+          {this.props.value}
+        </div>
+      );
+    }
+  }
+  
+  class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.isProgramer = this.isProgramer.bind(this);
+      this.state = {
+        knowsJava: null,
+        knowsPython: null
+      };
+    }
+  
+    isProgramer(select) {
+      if (select === "No") {
+        this.setState({ knowsJava: true, knowsPython: true });
+      } else {
+        this.setState({ knowsJava: null, knowsPython: null });
+      }
+    }
+  
+    render() {
+      return (
+        <div>
+          <h2>Have You Programed before?</h2>
+          <Radio name="programing" value="Yes" programer={this.isProgramer} />
+          <Radio name="programing" value="No" programer={this.isProgramer} />
+          <h2>Have You Programed in Java?</h2>
+          <Radio name="java" value="Yes" />
+          <Radio name="java" value="No" checked={this.state.knowsJava} />
+          <h2>Have You Programed in Python?</h2>
+          <Radio name="python" value="Yes" />
+          <Radio name="python" value="No" checked={this.state.knowsPython} />
+        </div>
+      );
+    }
+  }
+  
+  ReactDOM.render(<App />, document.getElementById("app"));
